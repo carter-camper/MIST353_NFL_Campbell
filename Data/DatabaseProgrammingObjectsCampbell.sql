5f4c0770d118c2d0ccee3d197eabc6d7687b1301
Carter <cc00117@mix.wvu.edu>
2026-02-17 13:26:44 -0500

    Created 3 required queries

--- Data/DatabaseProgrammingObjectsCampbell.sql
+++ Data/DatabaseProgrammingObjectsCampbell.sql
@@ -1,2 +1,19 @@
 -- 3 queries 
---1 each for ConferenceDivision and Team tables, and 1 join query
+-- 1 each for ConferenceDivision and Team tables, and 1 join query
+
+-- ConferenceDivision query (select all northern divisions)
+select c.conference, c.division
+from ConferenceDivision c
+where Division = 'North';
+
+-- Team query (selecting all teams in the AFC South)
+select t.tname, t.tcityState, t.tcolors
+from Team t
+where CDID = 3;
+
+-- Join query (selecting all teams and their conference/division)
+select t.tname, t.tcityState, c.conference, c.division
+from Team t 
+inner join ConferenceDivision c on c.cdid=t.cdid
+order by t.tname;
+
