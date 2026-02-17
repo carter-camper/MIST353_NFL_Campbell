8e3dd55f742f353e6e71ecdd3c96f942d0676a13
Carter <cc00117@mix.wvu.edu>
2026-02-17 01:48:46 -0500

    Updated tcolors

--- Data/InsertDataCampbell.sql
+++ Data/InsertDataCampbell.sql
@@ -1,16 +1,15 @@
 --Insert all ConferenceDivison Data (8 Rows)
 --Insert team data for AFC North (4 Rows)
-use MIST353_NFL_Campbell;
 
-insert into Team
+
+/*insert into Team
 values
 ('Ravens','Baltimore, MD','Purple','Logo',1),
 ('Bengals','Cincinnati, OH','Orange','Logo',1),
-('Browns','Cleveland, OH','Brown','Logo',1);
-
-Select * from Team;
+('Browns','Cleveland, OH','Brown','Logo',1);*/
 
 
+use MIST353_NFL_Campbell;
 
 
 
