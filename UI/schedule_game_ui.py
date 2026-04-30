2d85ab06fd9a32673b5c5244180f187cb02f68f7
carter <carter15102@gmail.com>
2026-04-29 23:31:56 -0400

    changed min value on schedule proc ui

--- UI/schedule_game_ui.py
+++ UI/schedule_game_ui.py
@@ -4,13 +4,13 @@ from fetch_data import post_data
 def schedule_game_ui():
     # Get user input for scheduling a game
     st.header("Schedule a New Game")
-    HomeTeamID = st.number_input("Enter Home Team ID:", min_value=1, step=1)
-    AwayTeamID = st.number_input("Enter Away Team ID:", min_value=1, step=1)
+    HomeTeamID = st.number_input("Enter Home Team ID:", min_value=None, step=1)
+    AwayTeamID = st.number_input("Enter Away Team ID:", min_value=None, step=1)
     GameRound = st.text_input("Enter Game Round:")
     GameDate = st.date_input("Enter Game Date:")
     GameStartTime = st.time_input("Enter Game Start Time:")
-    StadiumID = st.number_input("Enter Stadium ID:", min_value=1, step=1)
-    NFLAdminID = st.number_input("Enter NFL Admin ID:", min_value=1, step=1)
+    StadiumID = st.number_input("Enter Stadium ID:", min_value=None, step=1)
+    NFLAdminID = st.number_input("Enter NFL Admin ID:", min_value=None, step=1)
 
 
     
