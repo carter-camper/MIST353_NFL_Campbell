3a01f78499c8b5f8f4439cfbaa8ba0a29dc14d22
carter <carter15102@gmail.com>
2026-05-03 18:05:20 -0400

    added session state value to fan teams proc

--- UI/get_teams_for_specified_fan_ui.py
+++ UI/get_teams_for_specified_fan_ui.py
@@ -4,20 +4,18 @@ from fetch_data import fetch_data
 def get_teams_for_specified_fan_ui():
     st.subheader("Get Teams for Specified Fan")
 
+    if "app_user_id" not in st.session_state:
+        st.warning("Please validate the user first.")
+        return
 
-    nflfanid = st.text_input("Enter NFL Fan ID:")
+    fan_id = st.text_input("Fan ID", value=st.session_state.app_user_id, disabled=True)
+
     if st.button("Fetch Teams"):
+        input_params = {"NFLFanID": fan_id}
+        df = fetch_data("get_teams_for_specified_fan/", input_params)
 
-      input_params = {}
-      if not nflfanid.strip():
-          st.error("NFL Fan ID is required.")
-      else:
-          input_params["NFLFanID"] = nflfanid
-      # define function fetch_data
-      df = fetch_data("get_teams_for_specified_fan/", input_params)
-
-      if df is not None and not df.empty:
-            st.subheader(f"User {nflfanid}'s Teams:")
+        if df is not None and not df.empty:
+            st.subheader(f"User {fan_id}'s Teams:")
             st.dataframe(
                 df,
                 use_container_width=True,
@@ -26,5 +24,5 @@ def get_teams_for_specified_fan_ui():
                     "PrimaryTeam": st.column_config.CheckboxColumn("Primary Team")
                 }
             )
-      else:
-            st.info(f"No teams found for the specified user.")
+        else:
+            st.info("No teams found for the specified user.")
